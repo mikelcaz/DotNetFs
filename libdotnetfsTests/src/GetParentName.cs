c0275bd8be2ec038d98e49ba986cd341a294ee72
Mikel Cazorla Pérez <mikelcaz.dev@gmail.com>
2018-11-04 23:08:59 +0100

    Add tests for GetParentName (unix)

---
+++ libdotnetfsTests/src/GetParentName.cs
@@ -0,0 +1,73 @@
+using System;
+using Xunit;
+using DotNetFs;
+
+namespace DotNetFsTests
+{
+    // TODO:
+    // Naive platform-specific detection.
+
+    // TODO:
+    // Lines commented out belongs to the proper implementation.
+    // Below them there is always a line based on a naive trick.
+    // Replace them when the proper implementation is done.
+    public class GetParentName
+    {
+        [Theory]
+        [InlineData("/", "/")]
+        [InlineData("/", "/fileOrDirectory")]
+        // [InlineData("/", "/directory/")]
+        [InlineData("/directory/..", "/directory/")]
+        [InlineData("/parent", "/parent/fileOrDirectory")]
+        // [InlineData("/parent", "/parent/directory/")]
+        [InlineData("/parent/directory/..", "/parent/directory/")]
+        [InlineData("/parent/directory/./..", "/parent/directory/.")]
+        // [InlineData("/parent", "/parent/directory/././")]
+        [InlineData("/parent/directory/././..", "/parent/directory/././")]
+        // [InlineData("/parent", "/parent/directory/..")]
+        [InlineData("/parent/directory/../..", "/parent/directory/..")]
+        // [InlineData("/parent", "/parent/directory/../")]
+        [InlineData("/parent/directory/../..", "/parent/directory/../")]
+        public void UnixAbsolutePathTest(string expected, string path)
+        {
+            PathTest(expected, path, forWindows: false);
+        }
+
+        [Theory]
+        // [InlineData("..", ".")]
+        [InlineData("./..", ".")]
+        // [InlineData("..", "./")]
+        [InlineData("./..", "./")]
+        [InlineData(".", "./fileOrDirectory")]
+        // [InlineData(".", "./fileOrDirectory/")]
+        [InlineData("./fileOrDirectory/..", "./fileOrDirectory/")]
+        // [InlineData(".", "./fileOrDirectory/./.")]
+        [InlineData("./fileOrDirectory/././..", "./fileOrDirectory/./.")]
+        // [InlineData(".", "./fileOrDirectory/././")]
+        [InlineData("./fileOrDirectory/././..", "./fileOrDirectory/././")]
+        // [InlineData("./fileOrDirectory", "./fileOrDirectory/..")]
+        [InlineData("./fileOrDirectory/../..", "./fileOrDirectory/..")]
+        // [InlineData("./fileOrDirectory", "./fileOrDirectory/../")]
+        [InlineData("./fileOrDirectory/../..", "./fileOrDirectory/../")]
+        public void UnixRelativePathTest(string expected, string path)
+        {
+            PathTest(expected, path, forWindows: false);
+        }
+
+        internal void PathTest(
+            string expected,
+            string path,
+            bool forWindows
+        )
+        {
+            var isWindows =
+                Environment.OSVersion.Platform == PlatformID.Win32NT;
+
+            if (isWindows != forWindows)
+                return;
+
+            var parent = Fs.GetParentName(path);
+            Assert.Equal(expected, parent);
+        }
+    }
+}
