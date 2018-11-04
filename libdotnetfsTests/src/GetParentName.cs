0184ac247c5c9517a00f708be2fc00ffab7cd81a
Mikel Cazorla Pérez <mikelcaz.dev@gmail.com>
2018-11-04 23:37:14 +0100

    Add tests for GetParentName (windows)

--- libdotnetfsTests/src/GetParentName.cs
+++ libdotnetfsTests/src/GetParentName.cs
@@ -54,6 +54,75 @@ namespace DotNetFsTests
             PathTest(expected, path, forWindows: false);
         }
 
+        [Theory]
+        [InlineData(@"\", @"\")]
+        // It should not check the fs... but it does.
+        [InlineData(@"Z:\", @"Z:\")]
+        [InlineData(@"C:\", @"C:\")]
+        [InlineData(@"\", @"\fileOrDirectory")]
+        [InlineData(@"C:\", @"C:\fileOrDirectory")]
+        // [InlineData(@"\", @"\directory\")]
+        [InlineData(@"\directory\..", @"\directory\")]
+        [InlineData(@"\parent", @"\parent\fileOrDirectory")]
+        // [InlineData(@"\parent", @"\parent\directory\")]
+        [InlineData(@"\parent\directory\..", @"\parent\directory\")]
+        [InlineData(@"\parent\directory\.\..", @"\parent\directory\.")]
+        // [InlineData(@"\parent", @"\parent\directory\.\.\")]
+        [InlineData(@"\parent\directory\.\.\..", @"\parent\directory\.\.\")]
+        // [InlineData(@"\parent", @"\parent\directory\..")]
+        [InlineData(@"\parent\directory\..\..", @"\parent\directory\..")]
+        // [InlineData(@"\parent", @"\parent\directory\..\")]
+        [InlineData(@"\parent\directory\..\..", @"\parent\directory\..\")]
+        public void WindowsAbsolutePathTest(string expected, string path)
+        {
+            PathTest(expected, path, forWindows: true);
+        }
+
+        [Theory]
+        [InlineData("/", "/")]
+        // It should not check the fs... but it does.
+        [InlineData("Z:/", "Z:/")]
+        [InlineData("C:/", "C:/")]
+        [InlineData("/", "/fileOrDirectory")]
+        [InlineData("C:/", "C:/fileOrDirectory")]
+        // [InlineData("/", "/directory/")]
+        [InlineData("/directory/..", "/directory/")]
+        [InlineData("/parent", "/parent/fileOrDirectory")]
+        // [InlineData("/parent", "/parent/directory/")]
+        [InlineData("/parent/directory/..", "/parent/directory/")]
+        [InlineData("/parent/directory/./..", "/parent/directory/.")]
+        // [InlineData("/parent", "/parent/directory/././")]
+        [InlineData("/parent/directory/././..", "/parent/directory/././")]
+        // [InlineData("/parent", "/parent/directory/..")]
+        [InlineData("/parent/directory/../..", "/parent/directory/..")]
+        // [InlineData("/parent", "/parent/directory/../")]
+        [InlineData("/parent/directory/../..", "/parent/directory/../")]
+        public void WindowsAbsolutePathWithSlashesTest(string expected, string path)
+        {
+            PathTest(expected, path, forWindows: true);
+        }
+
+        [Theory]
+        // [InlineData(@"..", @".")]
+        [InlineData(@".\..", @".")]
+        // [InlineData(@"..", @".\")]
+        [InlineData(@".\..", @".\")]
+        [InlineData(@".", @".\fileOrDirectory")]
+        // [InlineData(@".", @".\fileOrDirectory\")]
+        [InlineData(@".\fileOrDirectory\..", @".\fileOrDirectory\")]
+        // [InlineData(@".", @".\fileOrDirectory\.\.")]
+        [InlineData(@".\fileOrDirectory\.\.\..", @".\fileOrDirectory\.\.")]
+        // [InlineData(@".", @".\fileOrDirectory\.\.\")]
+        [InlineData(@".\fileOrDirectory\.\.\..", @".\fileOrDirectory\.\.\")]
+        // [InlineData(@".\fileOrDirectory", @".\fileOrDirectory\..")]
+        [InlineData(@".\fileOrDirectory\..\..", @".\fileOrDirectory\..")]
+        // [InlineData(@".\fileOrDirectory", @".\fileOrDirectory\..\")]
+        [InlineData(@".\fileOrDirectory\..\..", @".\fileOrDirectory\..\")]
+        public void WindowsRelativePathTest(string expected, string path)
+        {
+            PathTest(expected, path, forWindows: true);
+        }
+
         internal void PathTest(
             string expected,
             string path,
