6a28c4357039c0a3b1ac3a6f91b5ba5847fae8bc
Mikel Cazorla Pérez <mikelcaz.dev@gmail.com>
2018-11-05 08:07:37 +0100

    Add tests for MustBelongToADirectory

---
+++ libdotnetfsTests/src/MustBelongToADirectory.cs
@@ -0,0 +1,96 @@
+using System;
+using Xunit;
+using DotNetFs;
+
+namespace DotNetFsTests
+{
+    public class MustBelongToADirectory
+    {
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/directory/")]
+
+        [InlineData(".")]
+        [InlineData("..")]
+        [InlineData("/directory/.")]
+        [InlineData("/directory/..")]
+
+        [InlineData("./")]
+        [InlineData("../")]
+        [InlineData("/directory/./")]
+        [InlineData("/directory/../")]
+        public void AbsolutePathBelongs(string path)
+        {
+            Assert.True(Path.MustBelongToADirectory(path));
+        }
+
+        [Theory]
+        [InlineData("directory/")]
+        [InlineData("./directory/")]
+        [InlineData("../directory/")]
+
+        [InlineData("directory/.")]
+        [InlineData("directory/..")]
+        [InlineData("./directory/.")]
+        [InlineData("../directory/..")]
+
+        [InlineData("directory/./")]
+        [InlineData("directory/../")]
+        [InlineData("./directory/./")]
+        [InlineData("../directory/../")]
+        public void RelativePathBelongs(string path)
+        {
+            Assert.True(Path.MustBelongToADirectory(path));
+        }
+
+        [Theory]
+        // File or directory. It should not check the fs... but it does.
+        [InlineData("/usr")]
+        [InlineData("/fileOrDirectory")]
+        public void AbsolutePathCouldBelong(string path)
+        {
+            Assert.False(Path.MustBelongToADirectory(path));
+        }
+
+        [Theory]
+        [InlineData("fileOrDirectory")]
+        [InlineData("directory/fileOrDirectory")]
+        [InlineData("./directory/fileOrDirectory")]
+        [InlineData("../directory/fileOrDirectory")]
+        public void RelativePathCouldBelong(string path)
+        {
+            Assert.False(Path.MustBelongToADirectory(path));
+        }
+
+        [Theory]
+        [InlineData(@"\")]
+        // It should not check the fs... but it does.
+        [InlineData(@"Z:\")]
+        [InlineData(@"C:\")]
+        public void BelongsOnWindowsToo(string path)
+        {
+            BelongsOnWindows(path);
+        }
+
+        [Theory]
+        [InlineData("/")]
+        // It should not check the fs... but it does.
+        [InlineData("Z:/")]
+        [InlineData("C:/")]
+        public void BelongsOnWindowsTooWithSlashes(string path)
+        {
+            BelongsOnWindows(path);
+        }
+
+        public void BelongsOnWindows(string path)
+        {
+            var isNotWindows =
+                Environment.OSVersion.Platform != PlatformID.Win32NT;
+
+            if (isNotWindows)
+                return;
+
+            Assert.True(Path.MustBelongToADirectory(path));
+        }
+    }
+}
