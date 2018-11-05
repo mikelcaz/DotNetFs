73702867bb31f37862b0079422036778c5faf53c
Mikel Cazorla Pérez <mikelcaz.dev@gmail.com>
2018-11-05 09:36:13 +0100

    Add tests for GetNormalized (non-Windows)
    Note the inconsistency with the placement of onlyforWindows
    (see GetParentName tests, there is a forWindows variable).
    Params arguments must be placed at the end,
    but in older versions of C#, named arguments must be placed there too.
    
    I had to do it this way to reduce verbosity.

---
+++ libdotnetfsTests/src/GetNormalized.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using DotNetFs;
+
+namespace DotNetFsTests
+{
+    // TODO:
+    // Naive platform-specific detection.
+    public class GetNormalized
+    {
+        [Theory]
+        [InlineData("/", "/.", "/./", "/..", "/../", "/directory/..")]
+        [InlineData(
+            "/directory",
+            "/directory/.", "/directory/otherDirectory/..",
+            "/directory/./", "/directory/otherDirectory/../")]
+        [InlineData(
+            "directory",
+            "./directory/.", "./directory/otherDirectory/..",
+            "./directory/./", "./directory/otherDirectory/../",
+            "directory/otherDirectory/..", "./directory/otherDirectory/..", "directory/./otherDirectory/..")]
+        public void NormalizationTest(params string[] paths)
+        {
+            NormalizationTestHelper(onlyForWindows: false, paths: paths);
+        }
+
+        internal void NormalizationTestHelper(
+            bool onlyForWindows,
+            params string[] paths
+        )
+        {
+            if (paths.Length < 2)
+                throw new ArgumentException("Paths cannot be compared if they are less than two.");
+
+            var isWindows =
+                Environment.OSVersion.Platform == PlatformID.Win32NT;
+
+            if (onlyForWindows && !isWindows)
+                return;
+
+            var normalized = new string[paths.Length - 1];
+            var referencePath = Path.GetNormalized(paths[0]);
+            for (int i = 1; i < paths.Length; i++)
+                normalized[i - 1] = Path.GetNormalized(paths[i]);
+
+            foreach (var path in normalized)
+                Assert.Equal(referencePath, path);
+        }
+    }
+}
