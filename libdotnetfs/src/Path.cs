a511f4157ca84aaa895aec167eda91c96effe9ad
Mikel Cazorla Pérez <mikelcaz.dev@gmail.com>
2018-11-05 09:49:14 +0100

    Implement GetNormalized

--- libdotnetfs/src/Path.cs
+++ libdotnetfs/src/Path.cs
@@ -1,6 +1,8 @@
 using System;
 using System.IO;
 
+using OldPath = System.IO.Path;
+
 // Note: let it be case-sensitive!
 // Note: its exhaustivity remains to be seen.
 namespace DotNetFs
@@ -9,15 +11,15 @@ namespace DotNetFs
     {
         public static string GetParentName(string path)
         {
-            var candidate = System.IO.Path.GetDirectoryName(path);
+            var candidate = OldPath.GetDirectoryName(path);
 
             // It seems "a" root path.
             if (candidate == null)
-                return System.IO.Path.GetPathRoot(path);
+                return OldPath.GetPathRoot(path);
 
             // Note: it seems that methods from Path think
             // "." and ".." aren't directories.
-            var fileName = System.IO.Path.GetFileName(path);
+            var fileName = OldPath.GetFileName(path);
 
             // It has to be a directory path.
             if (
@@ -27,7 +29,7 @@ namespace DotNetFs
             )
             {
                 // TODO: Make the proper implementation.
-                return System.IO.Path.Combine(path, "..");
+                return OldPath.Combine(path, "..");
             }
 
             // It has no parent info.
@@ -43,39 +45,41 @@ namespace DotNetFs
 
         public static string GetNormalized(string path)
         {
-            throw new NotImplementedException("GetNormalized not implemented");
+            return OldPath.GetFullPath(path).TrimEnd(
+                OldPath.DirectorySeparatorChar,
+                OldPath.AltDirectorySeparatorChar);
         }
 
         public static bool MustBelongToADirectory(string path)
         {
             return path == "."
                 || path == ".."
-                || path.EndsWith($"{System.IO.Path.DirectorySeparatorChar}")
-                || path.EndsWith($"{System.IO.Path.DirectorySeparatorChar}.")
-                || path.EndsWith($"{System.IO.Path.DirectorySeparatorChar}..")
-                || path.EndsWith($"{System.IO.Path.AltDirectorySeparatorChar}")
-                || path.EndsWith($"{System.IO.Path.AltDirectorySeparatorChar}.")
-                || path.EndsWith($"{System.IO.Path.AltDirectorySeparatorChar}..");
+                || path.EndsWith($"{OldPath.DirectorySeparatorChar}")
+                || path.EndsWith($"{OldPath.DirectorySeparatorChar}.")
+                || path.EndsWith($"{OldPath.DirectorySeparatorChar}..")
+                || path.EndsWith($"{OldPath.AltDirectorySeparatorChar}")
+                || path.EndsWith($"{OldPath.AltDirectorySeparatorChar}.")
+                || path.EndsWith($"{OldPath.AltDirectorySeparatorChar}..");
         }
 
         public static string GetFileName(string path)
         {
-            return System.IO.Path.GetFileName(path);
+            return OldPath.GetFileName(path);
         }
 
         public static string Combine(string path1, string path2)
         {
-            return System.IO.Path.Combine(path1, path2);
+            return OldPath.Combine(path1, path2);
         }
 
         public static string Combine(string path1, string path2, string path3)
         {
-            return System.IO.Path.Combine(path1, path2, path3);
+            return OldPath.Combine(path1, path2, path3);
         }
 
         public static string Combine(params string[] paths)
         {
-            return System.IO.Path.Combine(paths);
+            return OldPath.Combine(paths);
         }
     }
 }
