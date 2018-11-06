57b76a9b7a43d67b05210e86f308b4647a73bb9e
Mikel Cazorla Pérez <mikelcaz.dev@gmail.com>
2018-11-06 14:01:31 +0100

    Remove wrong/unsupported tests of GetParentNames
    Windows-specific tests with slashes.

--- libdotnetfsTests/src/Path/GetParentName.cs
+++ libdotnetfsTests/src/Path/GetParentName.cs
@@ -30,7 +30,7 @@ namespace DotNetFsTests
         [InlineData("/parent/directory/../..", "/parent/directory/../")]
         public void AbsolutePathTest(string expected, string path)
         {
-            PathTest(expected, path, forWindows: false);
+            ParentNamePathTest(expected, path, forWindows: false);
         }
 
         [Theory]
@@ -51,7 +51,7 @@ namespace DotNetFsTests
         [InlineData("./fileOrDirectory/../..", "./fileOrDirectory/../")]
         public void RelativePathTest(string expected, string path)
         {
-            PathTest(expected, path, forWindows: false);
+            ParentNamePathTest(expected, path, forWindows: false);
         }
 
         [Theory]
@@ -75,31 +75,7 @@ namespace DotNetFsTests
         [InlineData(@"\parent\directory\..\..", @"\parent\directory\..\")]
         public void WindowsAbsolutePathTest(string expected, string path)
         {
-            PathTest(expected, path, forWindows: true);
-        }
-
-        [Theory]
-        [InlineData("/", "/")]
-        // It should not check the fs... but it does.
-        [InlineData("Z:/", "Z:/")]
-        [InlineData("C:/", "C:/")]
-        [InlineData("/", "/fileOrDirectory")]
-        [InlineData("C:/", "C:/fileOrDirectory")]
-        // [InlineData("/", "/directory/")]
-        [InlineData("/directory/..", "/directory/")]
-        [InlineData("/parent", "/parent/fileOrDirectory")]
-        // [InlineData("/parent", "/parent/directory/")]
-        [InlineData("/parent/directory/..", "/parent/directory/")]
-        [InlineData("/parent/directory/./..", "/parent/directory/.")]
-        // [InlineData("/parent", "/parent/directory/././")]
-        [InlineData("/parent/directory/././..", "/parent/directory/././")]
-        // [InlineData("/parent", "/parent/directory/..")]
-        [InlineData("/parent/directory/../..", "/parent/directory/..")]
-        // [InlineData("/parent", "/parent/directory/../")]
-        [InlineData("/parent/directory/../..", "/parent/directory/../")]
-        public void WindowsAbsolutePathWithSlashesTest(string expected, string path)
-        {
-            PathTest(expected, path, forWindows: true);
+            ParentNamePathTest(expected, path, forWindows: true);
         }
 
         [Theory]
@@ -120,10 +96,10 @@ namespace DotNetFsTests
         [InlineData(@".\fileOrDirectory\..\..", @".\fileOrDirectory\..\")]
         public void WindowsRelativePathTest(string expected, string path)
         {
-            PathTest(expected, path, forWindows: true);
+            ParentNamePathTest(expected, path, forWindows: true);
         }
 
-        internal void PathTest(
+        internal void ParentNamePathTest(
             string expected,
             string path,
             bool forWindows
