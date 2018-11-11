50cda99b1f711ee6440a578269a1a0f537ddf5d5
Mikel Cazorla Pérez <mikelcaz.dev@gmail.com>
2018-11-11 18:47:05 +0100

    Implement the program

--- readpath/src/Program.cs
+++ readpath/src/Program.cs
@@ -1,5 +1,10 @@
+
 using System;
 using CommandLine;
+using DotNetFs;
+using DotNetFs.Io;
+
+using DirectoryInfo = System.IO.DirectoryInfo;
 
 namespace ReadPath
 {
@@ -8,12 +13,57 @@ namespace ReadPath
         static void Main(string[] args)
         {
             var parser = Parser.Default.ParseArguments<Options>(args)
-                .WithParsed(opt => {
-                    if(!opt.Anything)
-                        opt.Everything = true;
+                .WithParsed(opts => Processing(opts));
 
-                    Console.WriteLine(opt);
-                });
+        }
+
+        static void Processing(Options opts)
+        {
+            if (!opts.Anything)
+                opts.Everything = true;
+
+            foreach (var path in opts.Paths)
+            {
+                Console.WriteLine($"Input: {path.Quoted()}");
+
+                if (opts.Normalized)
+                {
+                    string normalized = Path.GetNormalized(path);
+                    Console.WriteLine($"Path.GetNormalized: {normalized.Quoted()}");
+                }
+
+                if (opts.AnyParentOption)
+                {
+                    string parentName = Path.GetParentName(path);
+                    if (opts.ParentName)
+                        Console.WriteLine($"Path.GetParentName: {parentName.Quoted()}");
+
+                    if (opts.NormalizedParentName)
+                    {
+                        var tag = (opts.ParentName)
+                            ? "(+ GetNormalized)"
+                            : "Path.GetParentName+GetNormalized";
+                        string parentNormalized = Path.GetNormalized(parentName);
+                        Console.WriteLine($"{tag}: {parentNormalized.Quoted()}");
+                    }
+                }
+
+                if (opts.Directory)
+                {
+                    string directory;
+                    try
+                    {
+                        var dirInfo = Directory.GetDirectory(path);
+                        directory = $"Exists: [{(dirInfo.Exists ? "YES" : "NO")}], {dirInfo.FullName.Quoted()}";
+                    }
+                    catch (ArgumentException e)
+                    {
+                        directory = e.Message;
+                    }
+                    Console.WriteLine($"Directory.GetDirectory: {directory}");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
